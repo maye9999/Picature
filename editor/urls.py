4c46922b43d8a8d2003585926f950b675382e4f8
lzhengning <lzhengning@gmail.com>
2016-05-31 22:12:51 +0800

    choose photo and editor

--- editor/urls.py
+++ editor/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .views import home, filters, editor
 urlpatterns = [
-    url(r'^$', editor),
+    url(r'^$', editor, name='editor'),
     url(r'^filters/(?P<fid>[0-9]+)$', filters),
 ]

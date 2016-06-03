9959bb2bd560a550e4f8919af7c3860e96264944
maye9999 <maye9999@163.com>
2016-06-03 15:33:06 +0800

    apply theme done

--- themes/urls.py
+++ themes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 from .views import themes
 urlpatterns = [
-    url(r'^(?P<tid>[0-9]+)/', themes),
+    url(r'^', themes),
 ]
--- themes/views.py
+++ themes/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
 
-def themes(request, tid):
-    pass
+def themes(request):
+    tid = request.GET['id']
+    theme = get_object_or_404(Theme, id=tid)
+    print(theme.operation)
+    return HttpResponse(theme.operation)

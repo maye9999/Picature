7657c0f59a14c1bf7a360839276c34b744d94d66
maye9999 <maye9999@163.com>
2016-06-03 21:32:45 +0800

    user url and timezone

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ urlpatterns = [
     url(r'^login', my_login),
     url(r'^logout', my_logout),
     url(r'^register', my_register),
-    url(r'^profile$', profile),
+    url(r'^(?P<uid>[0-9]+)/profile$', profile),
+    url(r'^profile$', home),
     url(r'^(?P<uid>[0-9]+)/images', profile),
     url(r'^(?P<uid>[0-9]+)/favorites', profile),
 ]
